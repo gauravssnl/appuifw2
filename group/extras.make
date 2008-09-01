bd28cdbbda50ed35c0e58afd5786c59475b77203
arkadiusz.wahlig <arkadiusz.wahlig@d722e5d8-1a54-0410-81c3-2588f0083ad8>
2008-09-01 17:53:13 +0000

    Initial commit using the new build system / IDE (Carbide C/C++).

---
+++ group/extras.make
@@ -0,0 +1,25 @@
+
+makmake:
+
+freeze:
+
+lib:
+
+cleanlib:
+
+clean:
+
+final:
+
+resource:
+
+bld: ..\python\appuifw2.pyc
+
+savespace:
+
+releasables:
+
+#==================================================
+
+..\python\appuifw2.pyc: ..\python\appuifw2.py
+	c:\python22\python.exe -O c:\python22\lib\compileall.py ..\python
---
+++ inc/appuifw2module.h
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name		: appuifw2.h
+ Author	  : Arkadiusz Wahlig
+ Copyright   : Copyright Arkadiusz Wahlig.
+ Description : appuifw2module.h - appuifw2 module header
+ ============================================================================
+ */
+
+#ifndef __APPUIFW2MODULE_H__
+#define __APPUIFW2MODULE_H__
+
+//  Include Files
+
+//  Constants
+
+//  Class Definitions
+
+#endif  // __APPUIFW2MODULE_H__
+
